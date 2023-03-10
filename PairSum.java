cdf687852e1e55b3173c7eec703c10b92d09d44d
Aboli Wankhade <104322603+Aboli56@users.noreply.github.com>
2023-03-10 21:15:42 +0530

    Update PairSum.java

--- PairSum.java
+++ PairSum.java
@@ -4,3 +4,34 @@
 // Example: For an input array [-4, 1, 3, -2, -1], 
 // one of the possible results would be 3, -2, -1 since their sum is 0.
 // Note: If there are more than 1 combination of such items, you can return any 1 of them.
+
+
+import java.util.Scanner;
+
+public class newOne{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int arr[] = new int[N];
+        for(int i=0;i<N;i++){
+            arr[i] = sc.nextInt();
+        }
+        int x = 0;
+        for(int i=0;i<N;i++) {
+            int sum=0;
+            for(int j=i;j<N;j++)
+            {
+                sum=sum+arr[j];
+                if(sum==0)
+                {
+                    x=1;
+                    break;
+                }
+            }
+        }
+        if(x==1)
+            System.out.println("Elements Found");
+        else
+            System.out.println("No Elements Found");
+    }
+    }
